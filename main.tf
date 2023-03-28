ddecb44583032e3cd663959caa7f7f478391e1b6
Dan Fedick <daniel.fedick@hashicorp.com>
2023-03-28 14:15:59 -0400

    id not ami_id

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ locals {
   // This AMI is the Ubuntu 20.04 located in us-east-2
   #my_ip           = var.my_ip
   cidr_block      = data.terraform_remote_state.vpc.outputs.vpc_cidr
-  ami_id          = data.aws_ami.ubuntu.ami_id
+  ami_id          = data.aws_ami.ubuntu.id
   private_subnets = data.terraform_remote_state.vpc.outputs.private_subnet_ids
   public_subnets  = data.terraform_remote_state.vpc.outputs.public_subnet_ids
 }
